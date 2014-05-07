ec2a6096526124f2331cbf6d0474cc1995445b49
lukemetz <luke.s.metz@gmail.com>
2014-05-07 09:27:16 -0400

    refactored into lib

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+#![crate_id = "raytrace_core"]
+//#![deny(missing_doc)]
+#![crate_type = "rlib"]
+pub mod camera;
+pub mod film;
+pub mod geometry;
+pub mod transform;
+pub mod sampler;
+pub mod sample;
+pub mod shape;
+pub mod spectrum;
+pub mod scene;
